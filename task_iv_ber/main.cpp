4970075d768b16f6920dba2b940b57cd1414ec61
mzielinski@ahe.lodz.pl <mzielinski@ahe.lodz.pl>
2022-04-09 09:08:16 +0200

    init commit. Ex 1

---
+++ task_iv_ber/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+
+int main(int argc, char * argv[])
+{
+    std::cout << "BER Calc v1.0" << std::endl;
+    std::cout << "argc =  : " << argc << std::endl; 
+    int iter = 0;
+    for (iter = 0; iter < argc; iter++) {
+        std::cout << "argv[" << argc << "] =" << argv[iter] << std::endl;
+    }
+
+    return 0;
+}
