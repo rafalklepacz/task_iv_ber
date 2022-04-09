9d1aa36b1d34e506629e7756afbc3009c7c93b9e
mzielinski@ahe.lodz.pl <mzielinski@ahe.lodz.pl>
2022-04-09 09:14:25 +0200

    argc replaced with iter

--- task_iv_ber/main.cpp
+++ task_iv_ber/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char * argv[])
     std::cout << "argc =  : " << argc << std::endl; 
     int iter = 0;
     for (iter = 0; iter < argc; iter++) {
-        std::cout << "argv[" << argc << "] =" << argv[iter] << std::endl;
+        std::cout << "argv[" << iter << "] =" << argv[iter] << std::endl;
     }
 
     return 0;
