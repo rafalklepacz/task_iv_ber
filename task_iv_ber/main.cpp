605f030f1754587b3096bd4d3041885eb4feee1e
Rafał Klepacz <rafal.klepacz@hotmail.com>
2022-04-09 23:48:45 +0200

    added input arguments validation

--- task_iv_ber/main.cpp
+++ task_iv_ber/main.cpp
@@ -1,13 +1,48 @@
-﻿#include "pch.h"
+﻿#include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int argc, char * argv[])
+#include "pch.h"
+
+int main(int argc, char *argv[])
 {
     std::cout << "BER Calc v1.0" << std::endl;
-    std::cout << "argc =  : " << argc << std::endl; 
+    std::cout << "argc = " << argc << std::endl;
     int iter = 0;
-    for (iter = 0; iter < argc; iter++) {
-        std::cout << "argv[" << iter << "] =" << argv[iter] << std::endl;
+    for (iter = 0; iter < argc; iter++)
+    {
+        std::cout << "argv[" << iter << "] = " << argv[iter] << std::endl;
     }
+
+    if (argc != 3)
+    {
+        std::cerr << "Invalid arguments number, two arguments expected!!!" << std::endl;
+        std::cerr << "Try enter the paths of the two files to compare: <file_path_1> <file_path_2>" << std::endl;
+        return -1;
+    }
+    
+    std::string firstFilePath(argv[1]);
+    std::ifstream firstFileStream(firstFilePath);
+    if (!firstFileStream)
+    {
+        std::cerr << "The file does not exist: " << firstFilePath << std::endl;
+        firstFileStream.close();
+        return -1;
+    }
+
+    std::string secondFilePath(argv[2]);
+    std::ifstream secondFileStream(secondFilePath);
+    if (!secondFileStream)
+    {
+        std::cerr << "The file does not exist: " << secondFilePath << std::endl;
+        secondFileStream.close();
+        return -1;
+    }
+
+    // TODO: 
+
+    firstFileStream.close();
+    secondFileStream.close();
 
     return 0;
 }
